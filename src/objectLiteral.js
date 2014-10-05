32c29a1f54820f6b2994ebe47e5170447c0f2c89
Ralph Collett <ralph.collett@gmail.com>
2014-10-05 10:15:54 +0100

    Object literal example.

---
+++ src/objectLiteral.js
@@ -0,0 +1,17 @@
+document.writeln('Hello, world!');
+
+var person = {
+	firstName: "Sally",
+	surname: "McGrew",
+	age: 67
+};
+
+for (value in person) {
+	document.writeln(value);
+	document.writeln(person[value]);
+	document.writeln(typeof person[value]);
+}
+
+document.writeln(person.firstName);
+person.surname = "NewSurname";
+document.writeln(person.surname);
