d4ca504540d0d728438656057c1f3e5976aa8b22
Ralph Collett <ralph.collett@gmail.com>
2014-10-05 11:05:42 +0100

    Global variable example.

---
+++ src/globalAbatement.js
@@ -0,0 +1,23 @@
+// Example of using one global variable for your application
+var MYAPP = {};
+
+MYAPP.stooge = {
+	"first-name": "Joe",
+	"last-name": "Howard"
+};
+
+
+MYAPP.flight = {
+	"airline": "Oceanic",
+	"number": 815,
+	"departure": {
+		IATA: "SYD",
+		time: "2004-09-22 14:55",
+		city: "Sydney"
+	},
+	"arrival": {
+		IATA: "LAX",
+		time: "2004-09-23 10:42",
+		city: "Los Angeles"
+	}
+};
